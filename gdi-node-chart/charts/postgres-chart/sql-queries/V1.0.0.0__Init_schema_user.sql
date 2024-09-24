b0fabdc38e1cea5e23bfaa6445658bb43525eceb
Ibukun Bello <ibukunoreofe@gmail.com>
2024-09-24 15:29:33 +0200

    Removed gdi-nodes

--- gdi-node-chart/charts/postgres-chart/sql-queries/V1.0.0.0__Init_schema_user.sql
+++
@@ -1,5 +0,0 @@
--- Creates user for the application and set the default schema:
-
-create user ${app_user} password '${app_pass}';
-
-alter role ${app_user} set search_path = ${flyway:defaultSchema};
